bcf6f132941ae96c9a66e0c439af876c919ff57f
ikalco <davidmontanolanza@gmail.com>
2021-01-13 09:08:13 -0600

    Got all the pictures working
    Now I can start actually making the pieces move. The graphics are basically done, just remember to do a title screen.

--- Classes/bishop.js
+++ Classes/bishop.js
@@ -1,9 +1,12 @@
 class Bishop extends Piece {
   constructor(color, pos) {
     super(color, pos);
+    this.image = chessPiecesImg.get(370.75, 80, 71.5, 74);
+    this.calcColor();
   }
 
   show() {
     super.show();
+    if (this.color == 0) this.image = chessPiecesImg.get(370.75, 186, 71.5, 74);
   }
 }

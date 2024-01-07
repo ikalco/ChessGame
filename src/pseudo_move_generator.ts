b26f63dff9132b9576453672127abcd8c557081d
ikalco <73481042+ikalco@users.noreply.github.com>
2024-01-07 17:20:43 -0600

    started doing pseudo legal move generation

---
+++ src/pseudo_move_generator.ts
@@ -0,0 +1,28 @@
+import { Move } from "./move";
+import { Piece } from "./piece";
+
+export namespace PseduoLegalMoveGenerator {
+    export function gen_pawn_moves(piece: Piece): Move[] {
+        return [];
+    }
+
+    export function gen_rook_moves(piece: Piece): Move[] {
+        return [];
+    }
+
+    export function gen_knight_moves(piece: Piece): Move[] {
+        return [];
+    }
+
+    export function gen_bishop_moves(piece: Piece): Move[] {
+        return [];
+    }
+
+    export function gen_king_moves(piece: Piece): Move[] {
+        return [];
+    }
+
+    export function gen_queen_moves(piece: Piece): Move[] {
+        return [];
+    }
+}
---
+++ tests/pseudo_move_generator.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, test } from '@jest/globals';
+import { PseduoLegalMoveGenerator } from '../src/pseudo_move_generator';
+import { Piece, PieceColor, PieceType } from '../src/piece';
+import { Move } from '../src/move';
+
+describe("Testing pseudo legal move generation for each piece type.", () => {
+    test("Black Pawn ♙.", () => {
+    });
+
+    test("White Pawn ♟.", () => {
+    });
+
+    test("Rook ♜.", () => {
+    });
+
+    test("Knight ♞.", () => {
+    });
+
+    test("Knight Edge ♞.", () => {
+    });
+
+    test("Bishop ♝.", () => {
+    });
+
+    test("King ♚.", () => {
+    });
+
+    test("Queen ♛.", () => {
+    });
+});

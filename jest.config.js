14e0b82cdd2bf24076629811168200a374338a83
ikalco <73481042+ikalco@users.noreply.github.com>
2024-01-31 13:47:54 -0600

    made jest work with new naming

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,16 @@
 export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    moduleNameMapper: {
+        "./piece.js": "./piece",
+        "./board.js": "./board",
+        "./fen_notation.js": "./fen_notation",
+        "./algebra_notation.js": "./algebra_notation",
+        "./board_factory.js": "./board_factory",
+        "./legal_move_generator.js": "./legal_move_generator",
+        "./pseudo_move_generator.js": "./pseudo_move_generator",
+        "./gui.js": "./gui",
+        "./move.js": "./move",
+        "./perft.js": "./perft"
+    }
 };

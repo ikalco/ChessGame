cdc2fd4caa53102daeed96e4ce1dfd39404a8e24
ikalco <73481042+ikalco@users.noreply.github.com>
2024-02-01 13:46:39 -0600

    fixed interpreting en passant moves in perft divide

--- src/perft.ts
+++ src/perft.ts
@@ -1,7 +1,8 @@
 import { Board } from "./board.js";
 import { LegalMoveGenerator } from "./legal_move_generator.js";
-import { Move } from "./move.js";
+import { Move, MoveType } from "./move.js";
 import { AlgebraNotation } from "./algebra_notation.js";
+import { PieceColor } from "./piece.js";
 
 export namespace Perft {
     export type perft_divide = {
@@ -44,6 +45,10 @@ export namespace Perft {
             board.unmove();
 
             result.num_positions += positions;
+
+            if (move.type == MoveType.EnPassant)
+                move.to_row += board.at(move.from_row, move.from_col).color == PieceColor.WHITE ? -1 : 1;
+
             result[AlgebraNotation.fromMoveSimple(move)] = positions;
         }
 
